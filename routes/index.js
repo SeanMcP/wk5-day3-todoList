34e4c8021d9b58cd5865cf3aec481ee02de558e2
Sean McPherson <seanmcp25@gmail.com>
2017-08-16 19:02:01 -0400

    Add items to opposite array

--- routes/index.js
+++ routes/index.js
@@ -3,32 +3,8 @@ const router = express.Router();
 
 // Provided in the assignment
 
-const todoList = [
-  {
-    task: "Wash the behind your ears",
-    done: true
-  },
-  {
-    task: "Climb a tree",
-    done: true
-  },
-  {
-    task: "Read your favorite book",
-    done: false
-  },
-  {
-    task: "Write a letter",
-    done: false
-  },
-  {
-    task: "Play a game",
-    done: false
-  },
-  {
-    task: "Sing for fun",
-    done: false
-  }
-];
+let todoList = ["Wash the behind your ears", "Climb a tree", "Read your favorite book", "Write a letter", "Play a game", "Sing for fun"];
+let doneList = ["Hug a friend", "Say 'good day!' to a stranger", "Fly a kite"];
 //
 // app.get("/", function (req, res) {
 //   res.render('index', { todos: todos });
@@ -42,13 +18,23 @@ const todoList = [
 // End provided code
 
 router.get('/', function(req, res){
-  res.render('list', {todoList: todoList});
+  res.render('list', {todo: todoList, done: doneList});
 });
 
-router.push('/', function(req, res){
-  console.log(req.body.task);
+router.post('/add', function(req, res){
+  todoList.push(req.body.task);
+  res.redirect('/');
 });
 
+router.post('/done', function(req, res){
+  doneList.push(req.body.task)
+  res.redirect('/');
+})
+
+router.post('/todo', function(req, res){
+  todoList.push(req.body.task)
+  res.redirect('/');
+})
 // router.post('/signup', function(req, res){
 //
 // });
