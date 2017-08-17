3ccbc1591dfbb3b7e73f75c95f1fe06d0a1cd58f
Sean McPherson <seanmcp25@gmail.com>
2017-08-16 20:35:30 -0400

    Fix typos; move items from to-do to done array

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-let todoList = ["Wash the behind your ears", "Climb a tree", "Read a new book", "Write a letter", "Play a game", "Sing for fun"];
-let doneList = ["Hug a friend", "Say 'good day!' to a stranger", "Fly a kite"];
+let todoList = ["Wash behind your ears", "Climb a tree", "Sing for fun"];
+let doneList = ["Hug a friend", "Read a new book", "Write a letter", "Play a game", "Say 'good day!' to a stranger", "Fly a kite"];
 
 router.get('/', function(req, res){
   res.render('list', {todo: todoList, done: doneList});
@@ -17,7 +17,7 @@ router.post('/add', function(req, res){
 
 router.post('/done', function(req, res){
   doneList.push(req.body.task)
-  todoList = todoList.filter(function(item) {
+  todoList = todoList.filter(function(item){
     return item !== req.body.task;
   });
   res.redirect('/');
